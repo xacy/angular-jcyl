0921790f84cfacea486efaae8d1217d34b7145f6
xacy <xacy@users.noreply.github.com>
2021-06-01 12:21:34 +0200

    Ejercicio 6

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudComponent implements OnInit {
 
-  solicitud = {nombre: "Javier"};
+  solicitud = {
+    nombre: "Javier",
+    apellidos: "Prueba"
+  };
 
   constructor() {
     setInterval(() => {
@@ -22,5 +25,19 @@ export class SolicitudComponent implements OnInit {
   logear(mensaje: any): void{
     console.log("Hola! "+mensaje);
   }
+  apellidos($event: KeyboardEvent) {
+    const element = $event.target as HTMLInputElement
+    this.solicitud.apellidos = element.value;
+    element.classList.remove("largo");
+    element.classList.remove("corto");
+    this.solicitud.apellidos.length>5?element.classList.add("largo"):element.classList.add("corto");
 
+  }
+  nombre($event: KeyboardEvent) {
+    const element = $event.target as HTMLInputElement
+    this.solicitud.nombre = element.value;
+    element.classList.remove("largo");
+    element.classList.remove("corto");
+    this.solicitud.nombre.length>5?element.classList.add("largo"):element.classList.add("corto");
+  }
 }
