fa35da7f0fc51730f5c518ce2b4cc0d9c970de7f
xacy <xacy@users.noreply.github.com>
2021-06-04 09:43:42 +0200

    Ejercicio 10

--- src/app/centro/centro.component.ts
+++ src/app/centro/centro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-centro',
@@ -7,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CentroComponent implements OnInit {
 
-  centro = {
-    nombreCentro: "IES García Lorca"
-  }
+  @Input()
+  centro: Centros = {nombreCentro: "Prueba",direccionCentro:"Valor defecto"};
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+export interface Centros{
+  nombreCentro:String;
+  direccionCentro:String;
 }
