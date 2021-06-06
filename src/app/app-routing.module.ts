8f6d6117921e7af7f82ffbd6d9b811ccec5ea544
xacy <xacy@users.noreply.github.com>
2021-06-06 17:19:25 +0200

    Ejercicio 16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListadoSolicitudesComponent} from "./listado-solicitudes/listado-solicitudes.component";
 
-const routes: Routes = [];
+import {ListadoCentrosComponent} from "./listado-centros/listado-centros.component";
+
+const routes: Routes = [
+  {path: '', component: ListadoSolicitudesComponent},
+  {path: 'centros', component: ListadoCentrosComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/listado-solicitudes/listado-solicitudes.component.ts
+++ src/app/listado-solicitudes/listado-solicitudes.component.ts
@@ -2,6 +2,7 @@ import { Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import {Component, Input, OnInit} from '@angular/core';
 import {Solicitud} from "../Solicitud";
+import {SolicitudesService} from "../solicitudes.service";
 
 @Component({
   selector: 'app-listado-solicitudes',
@@ -10,7 +11,21 @@ import {Solicitud} from "../Solicitud";
 })
 export class ListadoSolicitudesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private solicitudesService:SolicitudesService) {
+    solicitudesService.getSolicitudes().then((x: any) => {
+      console.log(x);
+      x.items.forEach((item:any,index:number) =>{
+        this.solicitudes.push({
+          nombre: item.fields.nombre,
+          apellidos: item.fields.apellidos,
+          nacimiento: item.fields.nacimiento || new Date(),
+          ayuda: item.fields.ayuda,
+          tipoSolicitud: item.fields.tipoSolicitud
+        });
+      });
+      console.log("tamaño:"+this.solicitudes.length);
+    });
+  }
 
   ngOnInit(): void {
   }
